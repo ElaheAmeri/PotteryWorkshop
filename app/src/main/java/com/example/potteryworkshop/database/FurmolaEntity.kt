7da826e9ff905609e007d15d259efd9ace5a79c4
Elahe Ameri <elahelastname@gmail.com>
2022-05-05 12:49:07 -0700

    add dataclass FurmolaEntity

---
+++ app/src/main/java/com/example/potteryworkshop/database/FurmolaEntity.kt
@@ -0,0 +1,18 @@
+package com.example.potteryworkshop.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "FurmolaTable")
+data class FurmolaEntity(
+                        @PrimaryKey(autoGenerate = true)
+                        @ColumnInfo(name="GlazeIngredients")
+                        val GlazeIngredients :String="",
+                        @ColumnInfo(name="code")
+                        val code :Int,
+                        @ColumnInfo(name="value")
+                        val value :Float,
+                        @ColumnInfo(name="description")
+                        val description :String="") {
+}
